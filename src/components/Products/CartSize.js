4724eeb5ceb90e2da91565af4e72375ab26f2e37
Hasmuddin Ansari <46985051+hasmuddinansari@users.noreply.github.com>
2024-07-01 14:37:35 +0530

    cartsize file added

--- src/components/Products/CartSize.js
+++ src/components/Products/CartSize.js
@@ -2,45 +2,18 @@ import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import useWebWorker from '../hooks/useWorker';
 
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function sumWithAsync(number) {
-    let sum = 0;
-
-    for (let current = 0; current < number; current++) {
-        sum += current;
-
-        if (current % 1000000 === 0) {
-            await delay(0); // Yield to the event loop every 1,000,000 iterations
-        }
-    }
-
-    return sum;
-}
-
 
 const CartSize = () => {
-    const allProductsCount = 5000 * 100 * 10000;
-    const [result, setResult] = useState()
-    // const { calculate, result } = useWebWorker()
-    function performComputation(iterations) {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                let result = 0;
+    const allProductsCount = 5000 * 100 * 10000
+    const [response, setResponse] = useState('...')
+    const { calculate, result } = useWebWorker()
 
-                for (let i = 0; i < iterations; i++) {
-                    result += i
-                }
-                resolve(result);
-            }, 0);
-        });
-    }
 
-    const handleCalculate = async () => {
-        sumWithAsync(allProductsCount).then(setResult);
+    const handleCalculate = () => {
+        setResponse('Calulating....')
+        calculate(allProductsCount)
     };
+
     return (
         <div>
             <h2>All products: {allProductsCount}</h2>
